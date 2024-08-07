3d5b422f4f417c8e0d4b68389510861edd7d3e5b
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-07 12:25:07 -0600

    Fix mobile overflow when showing navbar

--- js/main.js
+++ js/main.js
@@ -3,10 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const initialIconSrc = navbarToggler.src;
   const iconCloseSrc = "images/icon-close.svg";
 
+  const backdrop = document.getElementById("navbarContent");
+
   navbarToggler.addEventListener("click", function () {
     if (navbarToggler.src === initialIconSrc) {
       navbarToggler.src = iconCloseSrc;
+      document.body.classList.add("no-scroll");
     } else {
+      navbarToggler.src = initialIconSrc;
+      document.body.classList.remove("no-scroll");
+    }
+  });
+
+  backdrop.addEventListener("click", function (e) {
+    if (e.target.id === "navbarContent") {
+      this.classList.remove("show");
       navbarToggler.src = initialIconSrc;
     }
   });
